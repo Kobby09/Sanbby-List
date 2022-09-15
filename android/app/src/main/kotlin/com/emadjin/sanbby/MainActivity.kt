9bb355bda36e227cf4c4f726550a67cf1a6e7a0e
Kobby09 <emmanueladjei26@gmail.com>
2022-09-15 11:47:11 +0000

    My First Commit

---
+++ android/app/src/main/kotlin/com/emadjin/sanbby/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.emadjin.sanbby
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/data/services/storage/repository.dart
@@ -0,0 +1,10 @@
+import 'package:sanbby/app/data/models/task.dart';
+import 'package:sanbby/app/data/providers/task/provider.dart';
+
+class TaskRepository {
+  TaskProvider taskProvider;
+  TaskRepository({required this.taskProvider});
+
+  List<Task> readTasks() => taskProvider.readTasks();
+  void writeTasks(List<Task> tasks) => taskProvider.writeTasks(tasks);
+}
